5cb781d7dacc1cc766678c2f782fd84a6671b030
leavecoy <pandadeluxe@mail.ru>
2026-02-26 18:42:06 +0300

    5.5 Знакомство с gRPC протоколом. Практика работы с gRPC протоколом

--- grpc_course_server.py
+++ grpc_course_server.py
@@ -1,3 +1,5 @@
+from pydoc import describe
+
 import grpc
 from concurrent import futures
 import course_service_pb2
@@ -5,14 +7,15 @@ import course_service_pb2_grpc
 
 class CourseServiceServicer(course_service_pb2_grpc.CourseServiceServicer):
     def GetCourse(self, request, context):
-        course_id = request.course_id,
-        title = "Автотесты API",
-        description = "description"
-        return course_service_pb2.GetCourseResponse(course_id=course_id, title=title, description=description)
+        return course_service_pb2.GetCourseResponse(
+            course_id=request.course_id,
+            title="Автотесты API",
+            description="Будем изучать написание API автотестов"
+        )
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    course_service_pb2_grpc.add_CourseServiceServicer_to_server(CourseServiceServicer, server)
+    course_service_pb2_grpc.add_CourseServiceServicer_to_server(CourseServiceServicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     print("gRPC сервер запущен на порту 50051...")
