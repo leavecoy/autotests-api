70855eccbc197107c062c07f00431881f51f25e4
Romanov_RS <romanov_rs@mail.ru>
2026-02-26 17:30:41 +0300

    Промежуточная работа

--- grpc_client.py
+++ grpc_client.py
@@ -1,12 +1,14 @@
-from http.client import responses
-
 import grpc
 
 import user_service_pb2
 import user_service_pb2_grpc
+import day_info_service_pb2
+import day_info_service_pb2_grpc
 
 channel = grpc.insecure_channel('localhost:50051')
-stub = user_service_pb2_grpc.UserServiceStub(channel)
+stub_user = user_service_pb2_grpc.UserServiceStub(channel)
+stub_info = day_info_service_pb2_grpc.DayInfoServiceStub(channel)
 
-response = stub.GetUser(user_service_pb2.GetUserRequest(username="Alice"))
-print(response)
+response_user = stub_user.GetUser(user_service_pb2.GetUserRequest(username="Alice"))
+response_day_info = stub_info.GetInfo(day_info_service_pb2.GetDayInfoRequest(username="Alice"))
+print(response_user, response_day_info, sep="\n")
--- grpc_server.py
+++ grpc_server.py
@@ -2,15 +2,26 @@ import grpc
 from concurrent import futures
 import user_service_pb2
 import user_service_pb2_grpc
+import day_info_service_pb2
+import day_info_service_pb2_grpc
+from datetime import datetime, date
 
 class UserServiceServicer(user_service_pb2_grpc.UserServiceServicer):
     def GetUser(self, request, context):
         print(f"Получен запрос к методу GetUser от пользователя: {request.username}")
         return user_service_pb2.GetUserResponse(message=f"Привет, {request.username}")
 
+class DayInfoServiceServicer(day_info_service_pb2_grpc.DayInfoServiceServicer):
+    def GetInfo(self, request, context):
+        print(f"Получен запрос к методу GetInfo от пользователя {request.username}")
+        return day_info_service_pb2.GetDayInfoResponse(
+            info=f"Текущая дата: {date.today()}, время: {datetime.now().time()}"
+        )
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     user_service_pb2_grpc.add_UserServiceServicer_to_server(UserServiceServicer(), server)
+    day_info_service_pb2_grpc.add_DayInfoServiceServicer_to_server(DayInfoServiceServicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     print("gRPC сервер запущен на порту 50051...")
