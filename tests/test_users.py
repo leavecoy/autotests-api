c18e9ba3b91d6031855d47a121cf19bd6b8baeac
Romanov_RS <romanov_rs@mail.ru>
2026-05-14 17:05:08 +0300

    Промежуточная работа

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,7 +2,6 @@ from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from http import HTTPStatus
 from tools.assertions.base import assert_status_code
-
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response
 
