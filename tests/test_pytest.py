ab0d7efdf6a90606b9a0e8b2aad52992dfe5b68a
Romanov_RS <romanov_rs@mail.ru>
2026-05-14 14:53:51 +0300

    Промежуточная работа

---
+++ tests/test_pytest.py
@@ -0,0 +1,10 @@
+def test_first_try():
+    print('Hello world!')
+
+def test_assert_positive_case():
+    assert (2+2) == 4
+
+def test_assert_negative_case():
+    x = 5
+    y = 2
+    assert (y + y) == x,  "2 + 2 is not equals to 5"
